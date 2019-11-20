86a8f855646e732aa2a325b794ac98dbcb9ee1ba
william.chenta <william.chenta@gmail.com>
2019-11-20 08:37:07 -0300

    projeto inicial xamarin forms

---
+++ Aula 99 - Introdução Xamarin/ProjetoInicial/ProjetoInicial/ProjetoInicial/MainPage.xaml.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xamarin.Forms;
+
+namespace ProjetoInicial
+{
+    public partial class MainPage : ContentPage
+    {
+        public MainPage()
+        {
+            InitializeComponent();
+        }
+
+        private void Logar(object sender, EventArgs e)
+        {
+            Navigation.PushAsync(new DashBoard());
+        }
+    }
+}
